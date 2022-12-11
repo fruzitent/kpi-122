0404fe6a04c721d077972a265285d786890f4df1
fruzitent <fruzit@gmail.com>
2022-12-11 02:04:39 +0000

    task8

---
+++ q3_signals/lab02/src/task8.py
@@ -0,0 +1,27 @@
+import numpy as np
+from matplotlib import pyplot as plt
+from matplotlib.figure import Figure
+
+from src.common import Axis, Signal
+
+
+def main() -> None:
+    fig: Figure = plt.figure(figsize=(16, 10))  # type: ignore
+    ax: plt.Axes = fig.add_subplot()  # type: ignore
+
+    # TODO: resample at 125 Hz
+    tbi: Signal = Signal(
+        title="Traumatic Brain Injury",
+        xaxis=Axis(label="Time, s"),
+        yaxis=Axis(
+            label="Intracranial Pressure, mmHg",
+            samples=np.loadtxt("./assets/intracranial_pressure.txt"),
+        ),
+    )
+    tbi.plot(ax)
+
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
