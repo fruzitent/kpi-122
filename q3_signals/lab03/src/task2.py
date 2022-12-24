a9b41279dafdfb0bd46628b532bf163ae51ed7a8
fruzitent <fruzit@gmail.com>
2022-12-24 04:02:31 +0000

    extract plotting to separate function

--- q3_signals/lab03/src/task2.py
+++ q3_signals/lab03/src/task2.py
@@ -8,12 +8,11 @@ from src.task3 import gain, periods
 
 
 def main() -> None:
-    sample_rate: int = 256
-    time: int = 1
-
     amp: float = 1
     freq0: float = 10
     hshift: float = 0
+    sample_rate: int = 256
+    time: int = 1
     vshift: float = 0
 
     rng: np.random.Generator = np.random.default_rng()
@@ -26,18 +25,37 @@ def main() -> None:
     out1: npt.NDArray[np.float64] = lfilter(numerator, denumerator, inp0, zi=init)[0]
 
     with plt.style.context("seaborn"):
-        _, (ax0) = plt.subplots(figsize=(16, 5), ncols=1, nrows=1)
+        plot(timespan, inp0, out0, out1)
 
-        ax0.plot(timespan, inp0, label="Input")
-        ax0.plot(timespan, out0, label="Init: None")
-        ax0.plot(timespan, out1, label="Init: Rand", linestyle="dashed")
-        ax0.legend()
-        ax0.set_title("System Response")
-        ax0.set_xlabel("Time, s")
-        ax0.set_ylabel("Voltage, V")
+    stats(sample_rate, freq0, inp0, out0, out1)
 
-        plt.show()
 
+def plot(
+    timespan: npt.NDArray[np.float64],
+    inp0: npt.NDArray[np.float64],
+    out0: npt.NDArray[np.float64],
+    out1: npt.NDArray[np.float64],
+) -> None:
+    _, (ax0) = plt.subplots(figsize=(16, 5), ncols=1, nrows=1)
+
+    ax0.plot(timespan, inp0, label="Input")
+    ax0.plot(timespan, out0, label="Output: None")
+    ax0.plot(timespan, out1, label="Output: Rand", linestyle="dashed")
+    ax0.legend()
+    ax0.set_title("System Response")
+    ax0.set_xlabel("Time, s")
+    ax0.set_ylabel("Voltage, V")
+
+    plt.show()
+
+
+def stats(
+    sample_rate: int,
+    freq0: float,
+    inp0: npt.NDArray[np.float64],
+    out0: npt.NDArray[np.float64],
+    out1: npt.NDArray[np.float64],
+) -> None:
     cycle: int = round(sample_rate / freq0)
     inp0 = inp0[cycle:]
     out0 = out0[cycle:]
--- q3_signals/lab03/src/task4.py
+++ q3_signals/lab03/src/task4.py
@@ -34,39 +34,55 @@ def main() -> None:
     out5: npt.NDArray[np.float64] = lfilter(numerator, denumerator, inp3)
 
     with plt.style.context("seaborn"):
-        _, (ax0, ax1, ax2, ax3) = plt.subplots(figsize=(16, 24), ncols=1, nrows=4)
+        plot(timespan, inp0, inp1, inp2, inp3, out0, out1, out2, out3, out4, out5)
 
-        ax0.plot(timespan, inp0, label="In #0")
-        ax0.plot(timespan, out0, label="Out #0")
-        ax0.legend()
-        ax0.set_title("Signal #0 (3 Hz)")
-        ax0.set_xlabel("Time, s")
-        ax0.set_ylabel("Voltage, V")
 
-        ax1.plot(timespan, inp1, label="In #1")
-        ax1.plot(timespan, out1, label="Out #1")
-        ax1.legend()
-        ax1.set_title("Signal #1 (20 Hz)")
-        ax1.set_xlabel("Time, s")
-        ax1.set_ylabel("Voltage, V")
+def plot(
+    timespan: npt.NDArray[np.float64],
+    inp0: npt.NDArray[np.float64],
+    inp1: npt.NDArray[np.float64],
+    inp2: npt.NDArray[np.float64],
+    inp3: npt.NDArray[np.float64],
+    out0: npt.NDArray[np.float64],
+    out1: npt.NDArray[np.float64],
+    out2: npt.NDArray[np.float64],
+    out3: npt.NDArray[np.float64],
+    out4: npt.NDArray[np.float64],
+    out5: npt.NDArray[np.float64],
+) -> None:
+    _, (ax0, ax1, ax2, ax3) = plt.subplots(figsize=(16, 24), ncols=1, nrows=4)
 
-        ax2.plot(timespan, inp2, label="In #0 + In #1")
-        ax2.plot(timespan, out2, label="Out #0 + Out #1")
-        ax2.plot(timespan, out3, label="Out (#0 + #1)", linestyle="dashed")
-        ax2.legend()
-        ax2.set_title("Additivity property")
-        ax2.set_xlabel("Time, s")
-        ax2.set_ylabel("Voltage, V")
+    ax0.plot(timespan, inp0, label="In #0")
+    ax0.plot(timespan, out0, label="Out #0")
+    ax0.legend()
+    ax0.set_title("Signal #0 (3 Hz)")
+    ax0.set_xlabel("Time, s")
+    ax0.set_ylabel("Voltage, V")
 
-        ax3.plot(timespan, inp3, label="In #0 * scale")
-        ax3.plot(timespan, out4, label="Out #0 * scale")
-        ax3.plot(timespan, out5, label="Out (#0 * scale)", linestyle="dashed")
-        ax3.legend()
-        ax3.set_title("Homogenity property")
-        ax3.set_xlabel("Time, s")
-        ax3.set_ylabel("Voltage, V")
+    ax1.plot(timespan, inp1, label="In #1")
+    ax1.plot(timespan, out1, label="Out #1")
+    ax1.legend()
+    ax1.set_title("Signal #1 (20 Hz)")
+    ax1.set_xlabel("Time, s")
+    ax1.set_ylabel("Voltage, V")
 
-        plt.show()
+    ax2.plot(timespan, inp2, label="In #0 + In #1")
+    ax2.plot(timespan, out2, label="Out #0 + Out #1")
+    ax2.plot(timespan, out3, label="Out (#0 + #1)", linestyle="dashed")
+    ax2.legend()
+    ax2.set_title("Additivity property")
+    ax2.set_xlabel("Time, s")
+    ax2.set_ylabel("Voltage, V")
+
+    ax3.plot(timespan, inp3, label="In #0 * scale")
+    ax3.plot(timespan, out4, label="Out #0 * scale")
+    ax3.plot(timespan, out5, label="Out (#0 * scale)", linestyle="dashed")
+    ax3.legend()
+    ax3.set_title("Homogenity property")
+    ax3.set_xlabel("Time, s")
+    ax3.set_ylabel("Voltage, V")
+
+    plt.show()
 
 
 if __name__ == "__main__":
