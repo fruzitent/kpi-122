5498ff11189f802024dd4dc303b2268ef845e762
fruzitent <fruzit@gmail.com>
2023-01-02 07:29:26 +0000

    task3

---
+++ q3_methods/p01/src/task3_test.py
@@ -0,0 +1,28 @@
+from decimal import Decimal
+from typing import Self
+
+from src.task2 import BroadSense, NarrowSense
+
+
+class TestBroadSense(object):
+    def test_maximum_absolute_error(self: Self) -> None:
+        assert BroadSense("0.745").maximum_absolute_error == Decimal("0.001")
+        assert BroadSense("12.384").maximum_absolute_error == Decimal("0.001")
+        assert BroadSense("21.7").maximum_absolute_error == Decimal("0.1")
+
+    def test_maximum_relative_error(self: Self) -> None:
+        assert BroadSense("0.745").maximum_relative_error == Decimal("0.0014")
+        assert BroadSense("12.384").maximum_relative_error == Decimal("0.000081")
+        assert BroadSense("21.7").maximum_relative_error == Decimal("0.0047")
+
+
+class TestNarrowSense(object):
+    def test_maximum_absolute_error(self: Self) -> None:
+        assert NarrowSense("0.3648").maximum_absolute_error == Decimal("0.00005")
+        assert NarrowSense("0.4357").maximum_absolute_error == Decimal("0.00005")
+        assert NarrowSense("2.3445").maximum_absolute_error == Decimal("0.00005")
+
+    def test_maximum_relative_error(self: Self) -> None:
+        assert NarrowSense("0.3648").maximum_relative_error == Decimal("0.00014")
+        assert NarrowSense("2.3445").maximum_relative_error == Decimal("0.000022")
+        assert NarrowSense("0.4357").maximum_relative_error == Decimal("0.00012")
