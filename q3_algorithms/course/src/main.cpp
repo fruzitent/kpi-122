1ae4bac3ee79a3f92ec1ad054ade87e6fcea9390
fruzitent <fruzit@gmail.com>
2023-01-06 15:11:49 +0000

    define console and file classes

--- q3_algorithms/course/src/main.cpp
+++ q3_algorithms/course/src/main.cpp
@@ -1,6 +1,77 @@
+#include <cmath>
+#include <cstring>
 #include <iostream>
+#include <memory>
 
-int main() {
-    std::cout << "Hello, world!\n";
-    return 0;
+class Input {
+  public:
+    Input()                         = default;
+    Input(const Input &)            = default;
+    Input(Input &&)                 = default;
+    virtual ~Input()                = default;
+    Input &operator=(const Input &) = default;
+    Input &operator=(Input &&)      = default;
+
+    virtual void read() { std::cout << "Input Read\n"; }
+
+    virtual void save() {}
+};
+
+class ConsoleInput : public Input {
+  public:
+    void read() override { std::cout << "Console Read\n"; }
+
+    void save() override {}
+};
+
+class FileInput : public Input {
+  public:
+    void read() override { std::cout << "File Read\n"; }
+
+    void save() override {}
+};
+
+std::ostream &usage(std::ostream &out) {
+    out << "Description:\n";
+    out << "  This program approximates function using Maclaurin Series\n";
+    out << "\n";
+
+    out << "Usage:\n";
+    out << "  ./main [options] [--] <name>\n";
+    out << "\n";
+
+    out << "Arguments:\n";
+    out << "  name: path to csv-file\n";
+    out << "\n";
+
+    out << "Options:\n";
+    out << "  --help: show this message and exit\n";
+    return out;
+}
+
+int main(int argc, char **argv) {
+    if (argc < 2) {
+        usage(std::cerr) << "ERROR: Not enough arguments\n";
+        return 1;
+    }
+
+    if (std::strcmp(argv[1], "--help") == 0) {
+        usage(std::cout);
+        return 0;
+    }
+
+    if (std::strcmp(argv[1], "console") == 0) {
+        ConsoleInput input;
+        input.read();
+        return 0;
+    }
+
+    if (std::strcmp(argv[1], "file") == 0) {
+        FileInput input;
+        input.read();
+        return 0;
+    }
+
+    usage(std::cerr) << "ERROR: Unknown argument\n";
+    return 1;
 }
