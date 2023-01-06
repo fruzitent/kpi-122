5e6644f4a73b9cda4c94b8822bdfe17880214e27
fruzitent <fruzit@gmail.com>
2023-01-06 12:32:55 +0000

    do not need tests

--- q3_algorithms/course/fuzz_test/fuzz_tester.cpp
+++
@@ -1,9 +0,0 @@
-#include <cstdint>
-#include <cstdlib>
-
-extern "C" int LLVMFuzzerTestOneInput(
-    [[maybe_unused]] const uint8_t *Data,
-    [[maybe_unused]] size_t         Size
-) {
-    return 0;
-}
