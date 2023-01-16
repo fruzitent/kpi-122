9d0a206f4382d3114d2c0b3da8c6c43b4454a1df
fruzitent <fruzit@gmail.com>
2023-01-16 14:14:35 +0000

    q3_algorithms@p03

--- q3_algorithms/p03/src/task1.cpp
+++ q3_algorithms/p03/src/task1.cpp
@@ -1,36 +1,21 @@
 #include <iostream>
-#include <vector>
 
-void print(auto range) {
-    for (auto item : range) {
-        std::cout << item << " ";
+void fill(auto* array, std::size_t size) {
+    for (std::size_t i = 0; i < size; ++i) {
+        std::cout << "Enter #" << i << ": ";
+        std::cin >> array[i];
     }
-    std::cout << "\n";
-}
-
-void print(auto* arr, std::size_t size) {
-    for (auto i = 0; i < size; i++) {
-        std::cout << arr[i] << " ";
-    }
-    std::cout << "\n";
 }
 
 template<typename T>
-void merge(
-    T*          arr0,
-    std::size_t size0,
-    T*          arr1,
-    std::size_t size1,
-    T*          arr2,
-    bool        strict = false
-) {
+void merge(T* arr0, std::size_t size0, T* arr1, std::size_t size1, T* arr2, bool strict = false) {
     if (strict && size0 != size1) {
         throw std::invalid_argument("size0 != size1");
     }
 
-    auto it0 = 0;
-    auto it1 = 0;
-    auto it2 = 0;
+    std::size_t it0 = 0;
+    std::size_t it1 = 0;
+    std::size_t it2 = 0;
 
     while (it0 < size0 && it1 < size1) {
         arr2[it2++] = arr0[it0++];
@@ -46,65 +31,32 @@ void merge(
     }
 }
 
-template<typename T>
-std::vector<T>
-merge(std::vector<T> vec0, std::vector<T> vec1, bool strict = false) {
-    if (strict && vec0.size() != vec1.size()) {
-        throw std::invalid_argument("size0 != size1");
+void print(auto* arr, std::size_t size) {
+    for (std::size_t i = 0; i < size; i++) {
+        std::cout << arr[i] << " ";
     }
-
-    auto it0 = vec0.begin();
-    auto it1 = vec1.begin();
-
-    std::vector<T> res {};
-
-    while (it0 != vec0.end() && it1 != vec1.end()) {
-        res.push_back(*it0++);
-        res.push_back(*it1++);
-    }
-
-    while (it0 != vec0.end()) {
-        res.push_back(*it0++);
-    }
-
-    while (it1 != vec1.end()) {
-        res.push_back(*it1++);
-    }
-
-    return res;
+    std::cout << "\n";
 }
 
-void carray() {
-    std::size_t size0 = 5;
-    std::size_t size1 = 10;
-    std::size_t size2 = size0 + size1;
+int main() try {
+    static constexpr std::size_t size0 = 5;
+    static constexpr std::size_t size1 = 10;
+    static constexpr std::size_t size2 = size0 + size1;
 
-    auto* arr0 = new int[size0] {1, 3, 5, 7, 9};
-    auto* arr1 = new int[size1] {2, 4, 6, 8, 10, 11, 12, 13, 14, 15};
+    auto* arr0 = new int[size0] {};
+    auto* arr1 = new int[size1] {};
+    fill(arr0, size0);
+    fill(arr1, size1);
+
     auto* arr2 = new int[size2] {};
-
     merge(arr0, size0, arr1, size1, arr2);
     print(arr2, size2);
 
     delete[] arr0;
     delete[] arr1;
     delete[] arr2;
-}
-
-void stl_vector() {
-    std::vector<int> vec0 {1, 3, 5, 7, 9};
-    std::vector<int> vec1 {2, 4, 6, 8, 10, 11, 12, 13, 14, 15};
-
-    auto vec2 = merge(vec0, vec1);
-    print(vec2);
-
-    vec0.clear();
-    vec1.clear();
-    vec2.clear();
-}
-
-int main() {
-    carray();
-    stl_vector();
     return 0;
+} catch (const std::exception& error) {
+    std::cerr << "ERROR: " << error.what();
+    return 1;
 }
